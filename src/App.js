32bf3d5f5c66751f7aa023775eb0ec9c2d113ba9
Asileka <asilkatya@gmail.com>
2023-01-06 16:27:01 +0000

    added functional Hit button, adapted code for useState

--- src/App.js
+++ src/App.js
@@ -3,31 +3,43 @@ import { CCardBody } from "@coreui/react";
 import { CCardTitle } from "@coreui/react";
 import { CCardText } from "@coreui/react";
 import { CButton } from "@coreui/react";
+import React, { useState } from "react";
 import "@coreui/coreui/dist/css/coreui.min.css";
+
+let startDeck = [
+  { card: "1", suitsymbol: "♠", suit: "spades", id: 1, value: 1 },
+  { card: "5", suitsymbol: "♦", suit: "diamonds", id: 2, value: 5 },
+  { card: "K", suitsymbol: "♣", suit: "clubs", id: 3, value: 10 },
+  { card: "Q", suitsymbol: "♣", suit: "clubs", id: 4, value: 10 },
+];
+
 function App() {
-  let deck = [
-    { card: "1", suitsymbol: "♠", suit: "spades", id: 1, value: 1 },
-    { card: "5", suitsymbol: "♦", suit: "diamonds", id: 2, value: 5 },
-    { card: "K", suitsymbol: "♣", suit: "clubs", id: 3, value: 10 },
-  ];
-  const playerHand = [];
-  function getRandomCard(deckOfCards) {
-    const cardIndex = Math.floor(Math.random() * deckOfCards.length);
-    const randomCard = deckOfCards[cardIndex];
-    playerHand.push(randomCard);
-    deck.splice(cardIndex, 1);
-    return randomCard;
+  const [playerHand, setPlayerHand] = useState([]);
+  const [deck, setDeck] = useState(startDeck);
+
+  function getRandomCard() {
+    const cardIndex = Math.floor(Math.random() * deck.length);
+    const randomCard = deck[cardIndex];
+
+    setPlayerHand([...playerHand, randomCard]);
+    setDeck([...deck.filter((val) => val.id !== randomCard.id)]);
   }
 
-  getRandomCard(deck);
-  getRandomCard(deck);
+  if (playerHand.length < 2) {
+    getRandomCard(deck);
+    getRandomCard(deck);
+  }
 
   function hit() {
     getRandomCard(deck);
+    console.log("hellooooo");
   }
+
   return (
     <div className="App">
-      <CButton color="light">Hit</CButton>
+      <CButton color="light" onClick={hit} type="button">
+        Hit
+      </CButton>
       <CButton color="light">Stand</CButton>
       <div>
         {playerHand.map((x) => {
